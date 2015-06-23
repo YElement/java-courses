b9b88dbbcf8ff35fef60122a517850b945af1cae
YElement <y.element@gmail.com>
2015-06-23 15:40:16 +0300

    Изменение Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,13 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int f1 = Integer.valueOf(arg[0]);
+		double f2 = Double.valueOf(arg[1]);
+		double s = f1+f2;
+		System.out.println("Сумма = " + s);
+		s = f1*f2;
+		System.out.println("Произведение = " + s);
+		s = f1/f2;
+		System.out.println("Частное = " + s);
 	}
 }
