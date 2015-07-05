f852930cd4e41459b4363187ec8b565561cea2d3
YElement <y.element@gmail.com>
2015-07-05 14:44:22 +0300

    With IDEA

---
+++ src/test/java/ru/lesson/lessons/CalculatorTest.java
@@ -0,0 +1,26 @@
+package ru.lesson.lessons;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by u31047 on 04.07.2015.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        Calculator calculator = new Calculator();
+        calculator.add(1, 1);
+        assertEquals(2, calculator.getResult());
+    }
+
+    @Test
+    public void testDiv() throws Exception {
+        Calculator calculator = new Calculator();
+        calculator.div(10, 5);
+        assertEquals(2, calculator.getResult());
+    }
+}
